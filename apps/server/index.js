428d7aefb76aaa55d36dae065ab76675863c9906
LeTrucAn <letrucan096666@gmail.com>
2025-12-24 19:52:00 +0700

    config: Add helmet, morgan and refine CORS

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
+// Connect DB
+connectDB();
+ 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+//Helmet
+app.use(helmet());
+
+//Morgan
+app.use(morgan('dev'));
+
+//CORS
 app.use(cors({
     origin: '*', 
     credentials: true
@@ -16,6 +28,6 @@ app.get('/', (req, res) => {
     res.send('<h1>Server is running on Render! </h1>');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
 });
